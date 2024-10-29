7580e1a9bea06802a2273d3dbe2000d3c43f2a8b
Imene Kerboua <imenelydia.kr@gmail.com>
2024-10-29 13:02:37 +0100

    upadate script

--- scripts/mieb_datasets.py
+++ scripts/mieb_datasets.py
@@ -1,6 +1,10 @@
+from typing import Optional
+
 import loguru
 import requests
 import pandas as pd
+
+from datasets import load_dataset
 
 import mteb
 
@@ -10,7 +14,7 @@ SIZE__API_URL_BASE = "https://datasets-server.huggingface.co/size?dataset="
 STATS__API_URL_BASE = "https://datasets-server.huggingface.co/statistics?dataset="
 
 
-def query(url: str):
+def query(url: str) -> Optional[str]:
     try:
         logger.info(f"Querying {url}")
         response = requests.get(url)
@@ -37,24 +41,42 @@ def get_descriptive_stats_value(task, level0_key: str = "n_samples", split: str 
                     if level1_key and level1_key in n_samples_dict[split]:
                         return n_samples_dict[split][level1_key]
                     else:
-                        return "N/A"
+                        return "-"
                 return n_samples_dict[split]
-    return "N/A"
+    return "-"
 
-def get_dataset_stats(task: mteb.TaskMetadata):
+def get_avg_choices(task: mteb.TaskMetadata, split: str = "test"):
+    logger.info(f"Getting average choices for {task.metadata.name}")
+    data = load_dataset(task.metadata.dataset["path"])
+    if "choices" in data[split].features:
+        lengths = [len(choices) for choices in data[split]["choices"]]
+        return sum(lengths) / len(lengths)
+    return "-"
+
+def get_n_samples_per_split(task: mteb.TaskMetadata, split: str):
+    data = query(url=SIZE__API_URL_BASE+task.metadata.dataset["path"])
+    if data:
+        if data["size"]:
+            if data['size']['splits']:
+                for split in data['size']['splits']:
+                    if split["config"] == "default" and split['split'] == split:
+                        return split['num_rows']
+    return "-"
+
+def get_dataset_stats(task: mteb.TaskMetadata, split: str = "test"):
     dataset_name = task.metadata.dataset["path"]
-    data = query(url=STATS__API_URL_BASE+dataset_name+"&split=test&config=default")
+    data = query(url=STATS__API_URL_BASE+dataset_name+f"&split={split}&config=default")
     if data:
         if data["statistics"]:
             for stats in data["statistics"]:
                 if stats["column_type"] == "class_label":
                     return stats["column_statistics"]["n_unique"]
-    return "N/A"
+    return "-"
 
 def get_dataset_info(dataset: str):
     data = query(url=SIZE__API_URL_BASE+dataset)
 
-    num_qrels = num_corpus_elements = num_queries = "N/A"
+    num_qrels = num_corpus_elements = num_queries = "-"
     if data:
         if data["size"]:
             if data['size']['splits']:
@@ -79,7 +101,7 @@ def get_stats(task: mteb.TaskMetadata, key: str):
         "num_corpus_elements": "num_documents",
         "num_queries": "num_queries"
     }
-    if stats == "N/A":
+    if stats == "-":
         return get_descriptive_stats_value(task, level0_key="avg_character_length", split="test", level1_key=map_keys[key])
     return stats
 
@@ -111,14 +133,16 @@ data = [
     {
         "Task": task.metadata.name,
         "Type": task.metadata.type,
-        # "\# Samples Train": get_descriptive_stats_value(task, "n_samples", "train"),
+        "\# Samples Train": get_n_samples_per_split(task, "train"),
         # "\# Samples Val": get_n_samples_per_split(task, "n_samples", "validation"),
         "\# Samples Test": get_descriptive_stats_value(task, "n_samples", "test"),
-        "\# Queries": get_stats(task, "num_queries") if "Retrieval" in task.metadata.type else "N/A",
+        "\# Queries": get_stats(task, "num_queries") if "Retrieval" in task.metadata.type else "-",
         # get_descriptive_stats_value(task, "avg_character_length", "test", "num_queries"),
-        "\# Documents": get_stats(task, "num_corpus_elements") if "Retrieval" in task.metadata.type else "N/A",
-        "\# Qrels": get_stats(task, "num_qrels") if "Retrieval" in task.metadata.type else "N/A", 
-        "\# Labels": get_dataset_stats(task) if "Classification" in task.metadata.type else "N/A",
+        "\# Documents": get_stats(task, "num_corpus_elements") if "Retrieval" in task.metadata.type else "-",
+        "\# Qrels": get_stats(task, "num_qrels") if "Retrieval" in task.metadata.type else "-", 
+        "\# Labels": get_dataset_stats(task) if "Classification" in task.metadata.type else "-",
+        "Avg. \# Choices": get_avg_choices(task) if "Choice" in task.metadata.type else "-",
+        "Metric": task.metadata.main_score,
     }
     for task in tasks
 ]
@@ -131,17 +155,20 @@ df.set_index("Type", inplace=True)
 column_format = ''.join(['c'] * (len(df.columns) + 1))
 
 columns_per_task_type = {
-    "Retrieval": ["Task", "\# Samples Test", "\# Queries", "\# Documents", "\# Qrels"],
-    "Classification": ["Task", "\# Samples Test", "\# Labels"],
-    "Clustering": ["Task", "\# Samples Test", "\# Labels"],
+    "Retrieval": ["Task", "\# Queries", "\# Documents", "\# Qrels", "Avg. \# Choices", "Metric"],
+    "Classification": ["Task", "\# Samples Train", "\# Samples Test", "\# Labels", "Metric"],
+    "ZeroShotClassification": ["Task", "\# Samples Train", "\# Samples Test", "\# Labels", "Metric"],
+    "Clustering": ["Task", "\# Samples Train", "\# Samples Test", "\# Labels", "Metric"],
 }
 
 for type_ in df.index.unique():
-    logger.info(f"Saving {type_} tasksk to LaTeX table.")
+    logger.info(f"Saving {type_} tasks to LaTeX table.")
     selected_df = df.copy()
     
     if "Retrieval" in type_:
         selected_df = df[columns_per_task_type["Retrieval"]].copy()
+    elif "ZeroShotClassification" in type_:
+        selected_df = df[columns_per_task_type["ZeroShotClassification"]].copy()
     elif "Classification" in type_:
         selected_df = df[columns_per_task_type["Classification"]].copy()
     elif "Clustering" in type_:
@@ -149,7 +176,7 @@ for type_ in df.index.unique():
 
     latex_code = selected_df.loc[type_].to_latex(
         f"mieb_datasets_{type_}.tex",
-        # index=False,
+        index=False,
         caption=f'Datasets overview and metadata for {type_} tasks.',
         label=f'tab:datasets_{type_}',
         column_format=column_format)
